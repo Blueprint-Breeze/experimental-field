613357a83fef8fc15d511137b04e60aa2f21a183
yingpengsha <yingpengsha@kuaishou.com>
2024-04-17 15:03:22 +0800

    refactor: replace with dnd-kit

--- src/pages/example/drag-the-target-to-the-target-area/Page.tsx
+++ src/pages/example/drag-the-target-to-the-target-area/Page.tsx
@@ -1,65 +1,99 @@
-import React, { useState } from "react";
-import { DndProvider, useDrop, useDrag } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { Cross2Icon } from "@radix-ui/react-icons";
+import React, { HTMLAttributes, Reducer, useReducer } from "react";
+import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
+import clsx from "clsx";
 
-interface DraggableProps {
-  disableDrag?: boolean;
-  clearable?: boolean;
-  onClear?: () => void;
-}
+const Box = React.forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(function Box(props, ref) {
+  const { className, children, ...otherProps } = props;
 
-const Draggable: React.FC<DraggableProps> = (props) => {
-  const { disableDrag = false, clearable = false, onClear = () => {} } = props;
-  const [, drag] = useDrag(() => ({
-    type: "draggable",
-  }));
   return (
-    <div
-      className="w-[300px] h-[300px] bg-white shadow-lg rounded-lg cursor-pointer"
-      ref={(node) => {
-        if (!disableDrag) {
-          drag(node);
-        }
-      }}
-    >
-      {clearable && (
-        <div className="w-full h-full flex items-center justify-center text-[80px]">
-          <Cross2Icon onClick={() => onClear()} width={"1em"} height={"1em"} />
-        </div>
-      )}
+    <div ref={ref} className={clsx("w-[300px]", "h-[300px]", "bg-white", "rounded-lg", className)} {...otherProps}>
+      {children}
     </div>
+  );
+});
+
+const Draggable: React.FC = () => {
+  const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
+    id: "draggable",
+  });
+
+  return (
+    <Box
+      className={clsx("transition-opacity", isDragging ? "shadow-lg" : "shadow-2xl")}
+      style={{
+        transform: CSS.Transform.toString(transform),
+      }}
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+    />
   );
 };
 
-const Droppable: React.FC = () => {
-  const [isDropped, setIsDropped] = useState(false);
-  const [, drop] = useDrop(() => ({
-    accept: "draggable",
-    drop: () => setIsDropped(true),
-  }));
+interface DroppableProps {
+  id: string;
+  onDrop?: () => void;
+  element: {
+    seatId: string;
+  };
+}
+
+const Droppable: React.FC<DroppableProps> = (props) => {
+  const { id, element } = props;
+  const isDropped = element.seatId === id;
+  const { setNodeRef } = useDroppable({
+    id,
+  });
 
   return (
     <div
-      className="w-[300px] h-[300px] box-content p-2 border-dashed border-4 border-gray-400 rounded-lg"
-      ref={(node) => {
-        drop(node);
-      }}
+      className={clsx(
+        "w-[300px]",
+        "h-[300px]",
+        "box-content",
+        "p-2",
+        "border-dashed",
+        "border-4",
+        !isDropped && "border-gray-400",
+        "rounded-lg"
+      )}
+      ref={setNodeRef}
     >
-      {isDropped ? <Draggable disableDrag clearable onClear={() => setIsDropped(false)} /> : null}
+      {id === element.seatId ? <Draggable /> : null}
     </div>
   );
 };
 
 const Page: React.FC = () => {
+  const [state, dispatch] = useReducer<Reducer<{ seatId: string }, { type: string }>>(
+    (state, action) => {
+      switch (action.type) {
+        case "to-left":
+          return { seatId: "left" };
+        case "to-right":
+          return { seatId: "right" };
+        default:
+          return state;
+      }
+    },
+    { seatId: "left" }
+  );
+
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndContext
+      onDragEnd={(event) => {
+        event.over?.id && dispatch({ type: `to-${event.over.id}` });
+      }}
+    >
       <div className="p-36 flex h-full w-full items-center justify-between">
-        <Draggable />
-        <div className="text-[3.23rem] text-nowrap">---&gt;</div>
-        <Droppable />
+        <Droppable id="left" element={state} onDrop={() => dispatch({ type: "to-left" })} />
+        <div className="text-[3.23rem] text-nowrap">
+          {state.seatId === "right" && "<"}---{state.seatId === "left" && ">"}
+        </div>
+        <Droppable id="right" element={state} onDrop={() => dispatch({ type: "to-right" })} />
       </div>
-    </DndProvider>
+    </DndContext>
   );
 };
 
--- src/pages/example/drag-the-target-to-the-target-area/with-animation/Page.tsx
+++ src/pages/example/drag-the-target-to-the-target-area/with-animation/Page.tsx
@@ -1,57 +1,64 @@
-import React, { Reducer, useReducer } from "react";
-import { DndProvider, useDrop, useDrag } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+import React, { HTMLAttributes, Reducer, useReducer } from "react";
+import { DndContext, useDraggable, useDroppable } from "@dnd-kit/core";
+import { CSS } from "@dnd-kit/utilities";
 import clsx from "clsx";
 
-const Draggable: React.FC = () => {
-  const [, drag] = useDrag(() => ({
-    type: "draggable",
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  }));
+const Box = React.forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(function Box(props, ref) {
+  const { className, children, ...otherProps } = props;
 
   return (
-    <div
-      className="w-[300px] h-[300px] bg-white shadow-lg rounded-lg cursor-pointer"
-      ref={(node) => {
-        drag(node);
+    <div ref={ref} className={clsx("w-[300px]", "h-[300px]", "bg-white", "rounded-lg", className)} {...otherProps}>
+      {children}
+    </div>
+  );
+});
+
+const Draggable: React.FC = () => {
+  const { attributes, listeners, setNodeRef, transform, isDragging } = useDraggable({
+    id: "draggable",
+  });
+
+  return (
+    <Box
+      className={clsx("transition-opacity", isDragging ? "shadow-lg" : "shadow-2xl")}
+      style={{
+        transform: CSS.Transform.toString(transform),
       }}
-    ></div>
+      ref={setNodeRef}
+      {...listeners}
+      {...attributes}
+    />
   );
 };
 
 interface DroppableProps {
   id: string;
+  onDrop?: () => void;
   element: {
     seatId: string;
   };
-  onDrop?: () => void;
 }
 
 const Droppable: React.FC<DroppableProps> = (props) => {
-  const { id, element, onDrop = () => {} } = props;
+  const { id, element } = props;
   const isDropped = element.seatId === id;
-  const [, drop] = useDrop(() => ({
-    accept: "draggable",
-    drop: onDrop,
-  }));
+  const { setNodeRef } = useDroppable({
+    id,
+  });
 
   return (
     <div
       className={clsx(
-        `w-[300px]`,
-        `h-[300px]`,
-        `box-content`,
-        `p-2`,
-        `border-dashed`,
-        `border-4`,
-        isDropped && `border-gray-400`,
-        `rounded-lg`
+        "w-[300px]",
+        "h-[300px]",
+        "box-content",
+        "p-2",
+        "border-dashed",
+        "border-4",
+        !isDropped && "border-gray-400",
+        "rounded-lg"
       )}
-      ref={(node) => {
-        drop(node);
-      }}
+      ref={setNodeRef}
     >
       {id === element.seatId ? <Draggable /> : null}
     </div>
@@ -74,7 +81,11 @@ const Page: React.FC = () => {
   );
 
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndContext
+      onDragEnd={(event) => {
+        event.over?.id && dispatch({ type: `to-${event.over.id}` });
+      }}
+    >
       <div className="p-36 flex h-full w-full items-center justify-between">
         <Droppable id="left" element={state} onDrop={() => dispatch({ type: "to-left" })} />
         <div className="text-[3.23rem] text-nowrap">
@@ -82,7 +93,7 @@ const Page: React.FC = () => {
         </div>
         <Droppable id="right" element={state} onDrop={() => dispatch({ type: "to-right" })} />
       </div>
-    </DndProvider>
+    </DndContext>
   );
 };
 
